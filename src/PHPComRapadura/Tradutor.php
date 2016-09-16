5b345f140a99ea79c6299a9a4fa92ef56a0b64e7
Alef Castelo <alefaraujocastelo@gmail.com>
2016-09-15 22:37:23 -0300

    correcao na identacao

--- src/PHPComRapadura/Tradutor.php
+++ src/PHPComRapadura/Tradutor.php
@@ -18,24 +18,28 @@ class Tradutor
             $this->termos = Yaml::parse(file_get_contents($this->caminhoDaTraducao));
     }
 
-    public function caminhoDaTraducao(){
+    public function caminhoDaTraducao()
+    {
         return $this->caminhoDaTraducao;
     }
 
-    public function traduz($termo, $parametros = []){
+    public function traduz($termo, $parametros = [])
+    {
         if(array_key_exists($termo, $this->termos))
             return vsprintf($this->termos[$termo], $parametros);
 
         return false;
     }
 
-    public function adicionaTermo($termo, $termoTraduzido){
+    public function adicionaTermo($termo, $termoTraduzido)
+    {
         $this->termos[$termo] = $termoTraduzido;
         $this->escreveTermosTraduzidos();
         return true;
     }
 
-    public function escreveTermosTraduzidos(){
+    public function escreveTermosTraduzidos()
+    {
         $termos = Yaml::dump($this->termos);
         file_put_contents($this->caminhoDaTraducao(), $termos);
     }
